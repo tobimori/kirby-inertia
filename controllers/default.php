01728e866223607c6cfc3dc7e2dc0b88e9ad5151
Tobias Möritz <tobias@moeritz.cc>
2024-06-26 15:10:37 +0200

    handle Inertia::route case in default controller

--- controllers/default.php
+++ controllers/default.php
@@ -4,8 +4,8 @@ use Kirby\Cms\Page;
 use tobimori\Inertia\Inertia;
 
 return function (Page $page) {
-	return Inertia::createResponse(
+	return $page->intendedTemplate()->name() !== 'route' ? Inertia::createResponse(
 		$page->intendedTemplate(),
 		$page->toArray()
-	);
+	) : null;
 };
